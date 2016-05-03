1425afd8ee0a1a2baf631d7a18d7d8a9983e8c87
M8Project <abozoul@gmail.com>
2016-05-03 20:16:44 +0200

    Create E01_CuentaLineas.java

---
+++ E01_CuentaLineas.java
@@ -0,0 +1,42 @@
+/*
+
+  Programa que cuenta las líneas de un fichero de texto
+
+*/
+
+import java.io.*;
+
+public class E01_CuentaLineas {
+
+  public static void main(String[] args) throws IOException {
+
+      String filename = "prueba.txt";
+      FileInputStream fitxer=null;
+      
+      try {
+      
+    	  fitxer = new FileInputStream(filename);
+      }
+      catch (FileNotFoundException e) {
+    	  System.err.println("No existeix el fitxer "+filename);
+    	  return;
+      }
+      catch (SecurityException e) {
+    	  System.err.println("No es tenen drets d'accés al fitxer "+filename);
+    	  return;
+      }
+      
+      BufferedReader text = new BufferedReader(new InputStreamReader(fitxer));
+
+      int cont = 0;
+      while ( text.ready()) {
+    	  text.readLine();
+          cont++;
+      }
+      text.close();
+      fitxer.close();
+
+      System.out.println( "El fichero " + filename + " tiene " + cont + " lineas");
+
+  }
+}
